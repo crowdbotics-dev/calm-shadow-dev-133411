3ef6c29ecfc2b87fd5290758ed14f9153fb1edbd
Crowdbotics <engineering@crowdbotics.com>
2024-02-13 23:17:31 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "calm-shadow-dev-133411",
+  url: "https://calm-shadow-dev-133411.botics.co",
+  api: "https://calm-shadow-dev-133411.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
